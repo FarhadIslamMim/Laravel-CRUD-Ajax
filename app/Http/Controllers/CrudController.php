885b682120a2ca5b024c2c1692a52e5f0dfc932e
Mim <farhad15-11090@diu.edu.bd>
2022-08-23 21:09:32 +0600

    API Validation in POST mathod done!

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -4,170 +4,177 @@ namespace App\Http\Controllers;
 
 use App\Models\Curd;
 use Session;
+use Validator;
 use Illuminate\Http\Request;
 
 class CrudController extends Controller
 {
 
-    public function showDataApi($id=null)
+    public function showDataApi($id = null)
     {
-        
-       return  $id?Curd::find($id):Curd::all();
+
+        return  $id ? Curd::find($id) : Curd::all();
     }
     public function addDataApi(Request $request)
     {
-        $curdtable=new Curd();
-        $curdtable->name= $request->name;
-        $curdtable->email= $request->email;
-        $curdtable->address= $request->address;
 
-        $result=$curdtable->save();
+        $rules = [
+            'name' => 'required|min:3|max:15',
+            'email' => 'required',
+            'address' => 'required|max:15',
+        ];
+        $validator = 'Validator'::make($request->all(), $rules);
+        //$this->validate($request->all(),$rules);
 
-        if($result){
-            return  ["Result"=>"Data Saved Successfully!"];
-        }
-        else{
-            return  ["Result"=>"Faield!"];
+        if ($validator->fails()) {
+            //return $validator->errors();  
+            //another error mag
+            return response()->json($validator->errors(),401);
+        } else {
+            $curdtable = new Curd();
+            $curdtable->name = $request->name;
+            $curdtable->email = $request->email;
+            $curdtable->address = $request->address;
+
+            $result = $curdtable->save();
+
+            if ($result) {
+                return  ["Result" => "Data Saved Successfully!"];
+            } else {
+                return  ["Result" => "Faield!"];
+            }
         }
     }
     public function updateDataApi(Request $request)
     {
-        $curdtable=Curd::find($request->id);
-        $curdtable->name= $request->name;
-        $curdtable->email= $request->email;
-        $curdtable->address= $request->address;
+        $curdtable = Curd::find($request->id);
+        $curdtable->name = $request->name;
+        $curdtable->email = $request->email;
+        $curdtable->address = $request->address;
 
-       $result= $curdtable->save();
+        $result = $curdtable->save();
 
-        if($result){
-            return  ["Result"=>"Data Saved Successfully!"];
+        if ($result) {
+            return  ["Result" => "Data Saved Successfully!"];
+        } else {
+            return  ["Result" => "Faield!"];
         }
-        else{
-            return  ["Result"=>"Faield!"];
-        }
-    
     }
-    public function deleteDataApi($id=null)
+    public function deleteDataApi($id = null)
     {
-        $curdtable= Curd::find($id);
+        $curdtable = Curd::find($id);
 
-        $result=$curdtable->delete();
+        $result = $curdtable->delete();
 
-        if($result){
-            return  ["Result"=>"Data Saved Successfully!".$id];
-        }
-        else{
-            return  ["Result"=>"Faield!"];
+        if ($result) {
+            return  ["Result" => "Data Saved Successfully!" . $id];
+        } else {
+            return  ["Result" => "Faield!"];
         }
     }
     public function searchDataApi($name)
     {
-        
-            $result =Curd::where("name","like","%".$name."%")->get();
-            if(count($result) > 0){
-                return $result;
-            }
-            else{
-                return  $name." is not found!!";
-            }
-        
-    
+
+        $result = Curd::where("name", "like", "%" . $name . "%")->get();
+        if (count($result) > 0) {
+            return $result;
+        } else {
+            return  $name . " is not found!!";
+        }
     }
 
 
     public function showData()
     {
-        $showData= Curd::all();
+        $showData = Curd::all();
 
         //pagination
         //$showData=Curd::paginate(5);
         //Another pagination
-        $showData=Curd::simplePaginate(5);
+        $showData = Curd::simplePaginate(5);
 
-        return view('show_data',compact('showData'));
+        return view('show_data', compact('showData'));
     }
 
     public function addData()
     {
         return view('add_data');
     }
-    
+
     public function storeData(Request $request)
     {
-        $rules=[
-            'name'=> 'required|max:15',
-            'email'=> 'required',
-            'address'=> 'required|max:15',
+        $rules = [
+            'name' => 'required|max:15',
+            'email' => 'required',
+            'address' => 'required|max:15',
         ];
 
-        $cm=[
-            'name.requires'=>'Enter your Name',
-            'name.max'=>'Your name must not be greater than 15 characters.',
-            'email.email'=>'Enter a Valid Email',
-            'address.requires'=>'Enter your Address',
-            'address.max'=>'Your Address must not be greater than 15 characters.',
+        $cm = [
+            'name.requires' => 'Enter your Name',
+            'name.max' => 'Your name must not be greater than 15 characters.',
+            'email.email' => 'Enter a Valid Email',
+            'address.requires' => 'Enter your Address',
+            'address.max' => 'Your Address must not be greater than 15 characters.',
         ];
-        $this->validate($request,$rules,$cm);
+        $this->validate($request, $rules, $cm);
 
         //Model name is Curd
 
-        $curd= new Curd();   
-        $curd->name=$request->name;
-        $curd->email=$request->email;
-        $curd->address=$request->address;
+        $curd = new Curd();
+        $curd->name = $request->name;
+        $curd->email = $request->email;
+        $curd->address = $request->address;
         $curd->save();
 
-        'Session'::flash('msg','Data Successfully Added!');
+        'Session'::flash('msg', 'Data Successfully Added!');
 
 
         return redirect('/data');
-        
-    }
-    
-    public function editData($id=null)
-    {
-        $editData= Curd::find($id);
-        return view('edit_Data',compact('editData'));
     }
 
-    public function updateData(Request $request,$id)
+    public function editData($id = null)
     {
-        $rules=[
-            'name'=> 'required|max:15',
-            'email'=> 'required',
-            'address'=> 'required|max:15',
+        $editData = Curd::find($id);
+        return view('edit_Data', compact('editData'));
+    }
+
+    public function updateData(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'required|max:15',
+            'email' => 'required',
+            'address' => 'required|max:15',
         ];
 
-        $cm=[
-            'name.requires'=>'Enter your Name',
-            'name.max'=>'Your name must not be greater than 15 characters.',
-            'email.email'=>'Enter a Valid Email',
-            'address.requires'=>'Enter your Address',
-            'address.max'=>'Your Address must not be greater than 15 characters.',
+        $cm = [
+            'name.requires' => 'Enter your Name',
+            'name.max' => 'Your name must not be greater than 15 characters.',
+            'email.email' => 'Enter a Valid Email',
+            'address.requires' => 'Enter your Address',
+            'address.max' => 'Your Address must not be greater than 15 characters.',
         ];
-        $this->validate($request,$rules,$cm);
+        $this->validate($request, $rules, $cm);
 
         //Model name is Curd
 
-        $curd=  Curd::find($id);   
-        $curd->name=$request->name;
-        $curd->email=$request->email;
-        $curd->address=$request->address;
+        $curd =  Curd::find($id);
+        $curd->name = $request->name;
+        $curd->email = $request->email;
+        $curd->address = $request->address;
         $curd->save();
 
-        'Session'::flash('msg','Data Successfully Updated!');
+        'Session'::flash('msg', 'Data Successfully Updated!');
 
 
         return redirect('/data');
-        
     }
 
 
-    public function deleteData($id=null)
+    public function deleteData($id = null)
     {
-        $deleteData= Curd::find($id);
+        $deleteData = Curd::find($id);
         $deleteData->delete();
-        'Session'::flash('msg','Data Successfully Deleted.');
+        'Session'::flash('msg', 'Data Successfully Deleted.');
         return redirect('/data');
     }
 }
