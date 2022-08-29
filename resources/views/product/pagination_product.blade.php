9d1ebb351b69c85e4659eafc7b15e97b5a52218d
Mim <farhad15-11090@diu.edu.bd>
2022-08-29 17:03:02 +0600

    Ajax Pagination Done Complete

--- resources/views/product/pagination_product.blade.php
+++ resources/views/product/pagination_product.blade.php
@@ -1,43 +1,35 @@
+<a href="" class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#addModal">Add Product</a>
+@if(Session::has('msg'))
+<p class="alert alert-success">{{Session::get('msg')}}</p>
+@endif
 <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Product Name</th>
-                                <th scope="col">Product Size</th>
-                                <th scope="col">Product Price</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($products as $key=>$product)
-                            <tr>
-                                <td>{{$key+1}}</td>
-                                <td>{{$product->productName}}</td>
-                                <td>{{$product->productSize}}</td>
-                                <td>{{$product->productPrice}}</td>
-                                <td>
-                                    <a href=""
-                                            class="btn btn-success update_product_form"
-                                            data-bs-toggle="modal" 
-                                            data-bs-target="#updateModal"
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Product Size</th>
+            <th scope="col">Product Price</th>
+            <th scope="col">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($products as $key=>$product)
+        <tr>
+            <td>{{$key+1}}</td>
+            <td>{{$product->productName}}</td>
+            <td>{{$product->productSize}}</td>
+            <td>{{$product->productPrice}}</td>
+            <td>
+                <a href="" class="btn btn-success update_product_form" data-bs-toggle="modal" data-bs-target="#updateModal" data-id="{{ $product->id}}" data-name="{{$product->productName}}" data-size="{{$product->productSize}}" data-price="{{$product->productPrice}}">
+                    <i class="las la-edit"></i>
+                </a>
+                <a href="" class="btn btn-danger delete_product" data-id="{{ $product->id}}">
+                    <i class="las la-trash-alt"></i>
+                </a>
 
-                                            data-id="{{ $product->id}}"
-                                            data-name="{{$product->productName}}"
-                                            data-size="{{$product->productSize}}"
-                                            data-price="{{$product->productPrice}}"
-                                           >
-                                           <i class="las la-edit"></i>
-                                      </a>
-                                    <a href="" 
-                                    class="btn btn-danger delete_product"
-                                    data-id="{{ $product->id}}"
-                                             >
-                                    <i class="las la-trash-alt"></i>
-                                    </a>
-
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {!!$products ->links()!!}
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{!!$products ->links()!!}
